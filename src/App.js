2d7397c733587d1d272c4c919d30530a1952064c
Danny Chang <dannychachang@gmail.com>
2018-07-05 12:11:38 -0400

    love dogs

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class Hello extends Component {
     // make sure to return some UI
     return (
       <div>
-        <h1>WHAT</h1>
-        <p>I LOVE TIGERS</p>
-        <h3>Also it is time for the tea</h3>
+        <h1>Hello {this.props.name}!</h1>
+        <h3>You are {this.props.age} years old!</h3>
+        <h3>You love {this.props.animals[2]}!</h3>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './App.js';
 
+var person = {
+  personName: "Bushy",
+  personAge: 24,
+  favorites: [
+    "dogs",
+    "dawgs",
+    "doggs"
+  ]
+}
+
 ReactDOM.render(
-  <Hello />,
+  <Hello
+    name={person.personName}
+    age={person.personAge}
+    animals={person.favorites}
+  />,
   document.getElementById('root')
 )
